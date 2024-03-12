224ccd08e3e01695e4360f32b5bde2679fdeeb86
momoreg2002 <152940581+momoreg2002@users.noreply.github.com>
2024-03-12 12:02:29 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,14 +3,7 @@ mkdir -p ~/.streamlit/
 echo "\
 [server]\n\
 headless = true\n\
-port = \$PORT\n\
+port = $PORT\n\
 enableCORS = false\n\
-\n\
-[theme]\n\
-primaryColor = '\"#F63366\"'\n\
-backgroundColor = '\"#FFFFFF\"'\n\
-secondaryBackgroundColor = '\"#F0F2F6\"'\n\
-textColor = '\"#262730\"'\n\
-font = '\"sans serif\"'\n\
 \n\
 " > ~/.streamlit/config.toml
